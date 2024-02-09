baae7a1cce12f761f746cc4d469e3a1116d1aec1
Cheeseman757 <156013482+Cheeseman757@users.noreply.github.com>
2024-02-09 09:48:11 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -41,9 +41,9 @@ this.allProducts = productsArray.map(product => new Product(product.name, produc
 
   //this.instantiateProducts();
 
-function Product(name, fileExtension = 'jpg') {
+function Product(name, fileExtension = 'jpg', timesClicked = 0, timesShown = 0) {
   this.name = name;
   this.source = `assets/${name}.${fileExtension}`;
-  this.timesClicked = 0;
-  this.timesShown = 0;
+  this.timesClicked = timesClicked;
+  this.timesShown = timesShown;
 }
